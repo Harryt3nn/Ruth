ab7f318b4498966c7c6ae31d643cfcc0c345965a
Harry Tennant <c5030779@newcastle.ac.uk>
2026-04-02 02:33:57 +0100

    pt1: installation of openssl

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <openssl/sha.h>
 #include "khash.h"
-
 // hash map
 KHASH_MAP_INIT_STR(kvmap, char*)
 
