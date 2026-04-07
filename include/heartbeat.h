7d08feaf5361a60977685ab32cc51caf8da6b288
Harryt3nn <harryt3nn@gmail.com>
2026-04-07 01:28:35 +0100

    update: added comments to heartbeat.h and heartbeat.c

--- include/heartbeat.h
+++ include/heartbeat.h
@@ -1,18 +1,16 @@
 #ifndef HEARTBEAT_H
 #define HEARTBEAT_H
-
 #include "cluster.h"
 #include <pthread.h>
+#define HEARTBEAT_INTERVAL_MS 500 // 500 ms interval between heartbeats
+#define DEAD_THRESHOLD_MS 1500 // if node has been silent for 3 times heartbeat interval, it is considered dead
 
-#define HEARTBEAT_INTERVAL_MS 500
-#define DEAD_THRESHOLD_MS     1500
-
-typedef struct {
-    Cluster   *cluster;
-    int        running;
-    pthread_t  thread;
-    // Last time we heard from each node (ms timestamp)
-    long       last_seen[MAX_NODES];
+typedef struct 
+{
+    Cluster *cluster; // cluster state pointer 
+    int running; // flag
+    pthread_t thread; // Last time we heard from each node (ms timestamp)
+    long last_seen[MAX_NODES]; // last hearbeat stored here
 } HeartbeatMonitor;
 
 void heartbeat_start(HeartbeatMonitor *hb, Cluster *c);
--- src/heartbeat.c
+++ src/heartbeat.c
@@ -5,26 +5,44 @@
 #include <unistd.h>
 #include <time.h>
 
-static long now_ms() {
+// failure detector for the cluster of nodes
+// every node sends periodic heartbeat messages 
+// this ensures nodes are active, or 'alive'
+
+static long now_ms() // monotonic clock
+{
     struct timespec t;
     clock_gettime(CLOCK_MONOTONIC, &t);
     return t.tv_sec * 1000 + t.tv_nsec / 1000000;
 }
 
-static void *monitor_thread(void *arg) {
+
+static void *monitor_thread(void *arg) 
+{
+    // thread wakes every heartbeat interval
     HeartbeatMonitor *hb = arg;
-    while (hb->running) {
+    while (hb->running) 
+    {
         usleep(HEARTBEAT_INTERVAL_MS * 1000);
         long now = now_ms();
-        for (int i = 0; i < hb->cluster->count; i++) {
+
+
+        for (int i = 0; i < hb->cluster->count; i++) // iterates for all nodes
+        {
             Node *n = &hb->cluster->nodes[i];
-            if (hb->last_seen[i] == 0) continue;
-            long silent = now - hb->last_seen[i];
-            if (silent > DEAD_THRESHOLD_MS && n->status == NODE_ALIVE) {
+            if (hb->last_seen[i] == 0) continue; // (can skip inactive nodes)
+            long silent = now - hb->last_seen[i]; // silence duration for nodes
+
+            // detect dead nodes (inactivity for 3x heartbeat interval duration)
+            if (silent > DEAD_THRESHOLD_MS && n->status == NODE_ALIVE) 
+            {
                 cluster_mark_dead(hb->cluster, n->id);
-                printf("[heartbeat] node %d declared DEAD (%ldms silent)\n",
-                       n->id, silent);
-            } else if (silent <= DEAD_THRESHOLD_MS && n->status == NODE_DEAD) {
+                printf("[heartbeat] node %d declared DEAD (%ldms silent)\n",n->id, silent);
+            } 
+
+            // dead nodes can be brought back from the dead!!
+            else if (silent <= DEAD_THRESHOLD_MS && n->status == NODE_DEAD) 
+            {
                 cluster_mark_alive(hb->cluster, n->id);
                 printf("[heartbeat] node %d is ALIVE again\n", n->id);
             }
@@ -33,22 +51,27 @@ static void *monitor_thread(void *arg) {
     return NULL;
 }
 
-void heartbeat_start(HeartbeatMonitor *hb, Cluster *c) {
-    memset(hb, 0, sizeof(HeartbeatMonitor));
-    hb->cluster = c;
-    hb->running = 1;
-    pthread_create(&hb->thread, NULL, monitor_thread, hb);
+
+void heartbeat_start(HeartbeatMonitor *hb, Cluster *c) 
+{
+    memset(hb, 0, sizeof(HeartbeatMonitor)); // zeroises the heartbeat monitor struct
+    hb->cluster = c; // initialise the thread pointer
+    hb->running = 1; // monitor marked as running (node is alive)
+    pthread_create(&hb->thread, NULL, monitor_thread, hb); 
 }
 
-void heartbeat_stop(HeartbeatMonitor *hb) {
+void heartbeat_stop(HeartbeatMonitor *hb) 
+{
     hb->running = 0;
-    pthread_join(hb->thread, NULL);
+    pthread_join(hb->thread, NULL); // signals thread to exit
 }
 
-void heartbeat_update(HeartbeatMonitor *hb, int node_id) {
-    for (int i = 0; i < hb->cluster->count; i++)
-        if (hb->cluster->nodes[i].id == node_id) {
-            hb->last_seen[i] = now_ms();
+void heartbeat_update(HeartbeatMonitor *hb, int node_id) // call when heartbeat arrives from node
+{
+    for (int i = 0; i < hb->cluster->count; i++) // search for node in cluster
+        if (hb->cluster->nodes[i].id == node_id) 
+        {
+            hb->last_seen[i] = now_ms(); // update last seen, as a new update has just been sent
             return;
         }
 }
